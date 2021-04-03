d25154d34dbd7f0af9841a749c2e5c667bc7b315
huayun321 <huayun321@gmail.com>
2021-04-04 04:39:59 +0800

    add swap

---
+++ lib/swap.go
@@ -0,0 +1,88 @@
+package lib
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/huayun321/bot/global"
+	"github.com/huayun321/bot/pancake/router"
+	"log"
+	"math/big"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Swap struct {
+	mu sync.Mutex
+	b          *Bot
+	cid        *big.Int
+	sc         *swapConfig
+	nonce      uint64
+	privateKey *ecdsa.PrivateKey
+	public     common.Address
+	gasPrice   *big.Int
+	router *router.Router
+}
+
+func newSwap(b *Bot) *Swap {
+	cid, err := b.rpc.ChainID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	privateKey, err := crypto.HexToECDSA(global.AccountSetting.Private)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := b.rpc.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	address := common.HexToAddress(global.ContractSetting.Router)
+	instance, err := router.NewRouter(address, b.rpc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := &Swap{
+		b:          b,
+		cid:        cid,
+		sc:         b.swapConfig,
+		nonce:      nonce,
+		privateKey: privateKey,
+		public:     fromAddress,
+		gasPrice:   b.swapConfig.Price,
+		router: instance,
+	}
+	return s
+}
+
+func (s *Swap) startTx(amountIn, amountOut *big.Int, path []common.Address) {
+	atomic.AddUint64(&s.nonce, 1)
+	auth, err := bind.NewKeyedTransactorWithChainID(s.privateKey, s.cid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth.Nonce = big.NewInt(int64(s.nonce))
+	auth.Value = big.NewInt(0)               // in wei
+	auth.GasLimit = uint64(360000) // in units
+	auth.GasPrice = s.gasPrice
+
+	deadLine := time.Now().Unix() + 10
+	dlb := big.NewInt(deadLine)
+
+	tx, err := s.router.SwapExactTokensForTokens(auth, amountIn, amountOut, path, s.public, dlb)
+	if err != nil {
+		log.Println("SwapExactTokensForTokens", err)
+		return
+	}
+	log.Println(tx.Hash())
+}
